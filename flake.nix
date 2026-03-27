729c1767c46fb54caacf9dae2f59af303acc67b5
Vinicius Mayrink <vmayrink12@gmail.com>
2026-03-27 08:26:17 -0300

    feat: adds `fakeapi` CLI to the Nix flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    shell-utils.url = "github:vncsmyrnk/shell-utils";
-    shell-utils.inputs.nixpkgs.follows = "nixpkgs";
+    shell-utils = {
+      url = "github:vncsmyrnk/shell-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fakeapi = {
+      url = "github:vncsmyrnk/fakeapi";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, shell-utils }:
-    let system = "x86_64-linux";
-    in { packages.${system}.default = shell-utils.packages.${system}.default; };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      shell-utils,
+      fakeapi,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system} = {
+        default = pkgs.symlinkJoin {
+          name = "all-packages";
+          paths = [
+            shell-utils.packages.${system}.default
+            fakeapi.packages.${system}.default
+          ];
+        };
+      };
+    };
 }
