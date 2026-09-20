f66f9f5555503b34bc4ad65745d9c8099da0f129
Vinicius Mayrink <vmayrink12@gmail.com>
2026-09-19 21:21:09 -0300

    refactor!: drops `dotfiles` update manager
    fix drop

--- bin/dotfiles-update/repo.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type repoStrategy interface {
-	Poll(path string) (hasUpdates bool, err error)
-	Update(path string) error
-	Upgrade(path string) error
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/vncsmyrnk/dotfiles
-
-go 1.26.4
-
-require github.com/spf13/pflag v1.0.10
